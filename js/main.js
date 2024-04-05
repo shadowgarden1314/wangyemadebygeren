af1090816b06eb3935ab8892ccd84836fe853305
shadowgarden1314 <131418013+shadowgarden1314@users.noreply.github.com>
2024-04-05 23:21:01 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,9 +130,9 @@ $('#hitokoto').click(function () {
 //获取天气
 //请前往 https://www.mxnzp.com/doc/list 申请 app_id 和 app_secret
 //请前往 https://dev.qweather.com/ 申请 key
-const add_id = "vcpmlmqiqnjpxwq1"; // app_id
-const app_secret = "PeYnsesgkmK7qREhIFppIcsoN0ZShv3c"; // app_secret
-const key = "691d007d585841c09e9b41e79853ecc2" // key
+const add_id = "xomhhjnqrrbfgqhv"; // app_id
+const app_secret = "UEzNpuTgI1ymi2NktynIWXQi8AbVyFUU"; // app_secret
+const key = "63bb4c13a38f4bcf9532fbbf3cebce47" // key
 function getWeather() {
     fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
         .then(response => response.json())
